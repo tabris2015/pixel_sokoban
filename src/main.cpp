406c6a03999d4808951b9234217d8d3698d4632b
Jose Laruta <eduardo.laruta@gmail.com>
2020-01-26 18:29:51 -0400

    eliminado struct player

--- src/main.cpp
+++ src/main.cpp
@@ -31,39 +31,41 @@ class Board
 {
 private:
     std::vector<std::string> state;
-    Player player;
+    int px;
+    int py;
     int frame = 0;
 
     bool updatePlayer(int new_x, int new_y)
     {
-        if(state[new_y][new_x] == 'L')
+        if(state[new_y][new_x] == 'L' || state[new_y][new_x] == 'J')
         {
-            state[player.y][player.x] == 'L';
             state[new_y][new_x] = 'J';
-            player.x = new_x;
-            player.y = new_y;
+            state[py][px] = 'L';
+            px = new_x;
+            py = new_y;
             return true;
         }
         return false;
     };
 public:
-    Board(std::vector<std::string> init_state, Player i_player)
+    Board(std::vector<std::string> init_state, int x, int y)
     :
     state(init_state)
     {
-        initPlayer(i_player);
+        initPlayer(x, y);
     };
 
-    void initPlayer(Player new_player)
+    void initPlayer(int x, int y)
     {
-        if(!isValidPlayerXY(new_player.x, new_player.y)) 
+        if(!isValidPlayerXY(x, y)) 
         {
             std::cout << "posicion inicial del jugador Invalida" << std::endl;
             return;
         }
 
-        player = new_player;
-        updatePlayer(player.x, player.y);
+        px = x;
+        py = y;
+        updatePlayer(px, py);
     };
 
     void setState(std::vector<std::string> new_state)
@@ -88,7 +90,7 @@ public:
 
     bool isValidPlayerXY(int x, int y)
     {
-        if(x < 0 || y < 0 || x > (state[y].size() - 1) || 1 > (state.size() - 1)) 
+        if(x < 0 || y < 0 || x > (state[y].size() - 1) || y > (state.size() - 1)) 
             return false;
         return state[y][x] == 'L' || state[y][x] == 'J';
     };
@@ -98,23 +100,23 @@ public:
         switch (move)
         {
         case Movement::UP:
-            if(isValidPlayerXY(player.x, player.y - 1))
-                updatePlayer(player.x, player.y - 1);
+            if(isValidPlayerXY(px, py - 1))
+                updatePlayer(px, py - 1);
             break;
         
         case Movement::RIGHT:
-            if(isValidPlayerXY(player.x + 1, player.y))
-                updatePlayer(player.x + 1, player.y);
+            if(isValidPlayerXY(px + 1, py))
+                updatePlayer(px + 1, py);
             break;
         
         case Movement::DOWN:
-            if(isValidPlayerXY(player.x, player.y + 1))
-                updatePlayer(player.x, player.y + 1);
+            if(isValidPlayerXY(px, py + 1))
+                updatePlayer(px, py + 1);
             break;
         
         case Movement::LEFT:
-            if(isValidPlayerXY(player.x - 1, player.y))
-                updatePlayer(player.x - 1, player.y);
+            if(isValidPlayerXY(px - 1, py))
+                updatePlayer(px - 1, py);
             break;
         
         default:
@@ -131,7 +133,7 @@ int main(int argc, char const *argv[])
     std::cout << "hola sokoban" << std::endl;
     std::cout << "--------------" << std::endl;
     
-    Board b(board1, {2,2});
+    Board b(board1, 2, 2);
     b.draw();
 
     b.movePlayer(Movement::DOWN);
